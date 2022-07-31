c455aacb10373f112982d0aefde309009bee4fc9
Joy Wang <joywang912@hotmail.com>
2022-08-01 10:12:18 +1200

    excise 1-5

--- src/exercise/04.js
+++ src/exercise/04.js
@@ -4,6 +4,12 @@
 import * as React from 'react'
 import {Switch} from '../switch'
 
+const callAll =
+  (...fns) =>
+  (...args) => {
+    fns.forEach(fn => fn?.(...args))
+  }
+
 function useToggle() {
   const [on, setOn] = React.useState(false)
   const toggle = () => setOn(!on)
@@ -11,16 +17,31 @@ function useToggle() {
   // 🐨 Add a property called `togglerProps`. It should be an object that has
   // `aria-pressed` and `onClick` properties.
   // 💰 {'aria-pressed': on, onClick: toggle}
-  return {on, toggle}
+  // return {on, toggle, togglerProps: {'aria-pressed': on, onClick: toggle}}
+  return {
+    on,
+    toggle,
+    getTogglerProps: ({onClick, ...props} = {}) => ({
+      'aria-pressed': on,
+      onClick: callAll(onClick, toggle),
+      ...props,
+    }),
+  }
 }
 
 function App() {
-  const {on, togglerProps} = useToggle()
+  const {on, getTogglerProps} = useToggle()
   return (
     <div>
-      <Switch on={on} {...togglerProps} />
+      <Switch on={on} {...getTogglerProps()} />
       <hr />
-      <button aria-label="custom-button" {...togglerProps}>
+      <button
+        {...getTogglerProps({
+          'aria-label': 'custom-button',
+          onClick: e => console.info(e.target.id),
+          id: 'custom-button-id',
+        })}
+      >
         {on ? 'on' : 'off'}
       </button>
     </div>
